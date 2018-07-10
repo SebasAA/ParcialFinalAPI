37216246c0b3b29176a07e8087573d54cc33ca90
Douglas_HdezT <00004516@uca.edu.sv>
2018-07-10 16:43:50 -0600

    Ticket update

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -44,8 +44,41 @@ function insertTicket(req,res){
     });
 }
 
+function insertProduct(req, res){
+    let id_ticket = req.params.id_ticket;
+    let id_product_in = req.body.id_product;
+    let cant = req.body.cant;
+
+    if(id_product_in==null || id_product_in==""){
+        return res.status(404).send({
+            message:"material not found"
+        });
+    }
+
+    Ticket.findById(id_ticket, (err, ticket)=>{
+        if(err) return res.status(500).send({message:"Internal Server Error"});
+
+        ticket.shopping_cart.forEach((act) => {
+            if(act.id_product==id_product_){
+                ticket.shopping_cart.splice(ticket.shopping_cart.indexOf(act),1)
+            }
+        });
+
+        ticket.shopping_cart.push({id_product: id_product_in, quantity:cant});
+
+        ticket.save((err,ticketSave)=>{
+            if(err) return res.status(500).send({
+                message:"Internal Server Error"
+            });
+
+            res.status(200).send({message: "Inserted"})
+        });
+    });
+}
+
 module.exports = {
     getTicket,
     updateTicket,
-    insertTicket
+    insertTicket,
+    insertProduct
 }
--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -7,5 +7,6 @@ const api = express.Router();
 api.get("/ticket/:id_tickets", auth, TicketCtrl.getTicket);
 api.put("/ticket/:id_tickets", auth, TicketCtrl.updateTicket);
 api.post("/ticket", auth, TicketCtrl.insertTicket);
+api.post("/ticket/:id_ticket", auth, TicketCtrl.insertProduct);
 
 module.exports = api;
