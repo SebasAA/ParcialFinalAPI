46f84ad3a47a71dc7f5a8fc8685a96e6c3b2a248
Marcelo Martínez <marcelo2m1097@gmail.com>
2018-07-10 12:36:41 -0600

    Route for Material has been created

--- routes/materialRoutes.js
+++ routes/materialRoutes.js
@@ -1,2 +1,10 @@
 'use strict'
 const express = require("express");
+const MaterialCtrl = require('../controllers/materialController');
+const auth = require('../middleware');
+const api = express.Router();
+
+api.post("/material", auth, MaterialCtrl.insertMaterial);
+api.get("/material", auth, MaterialCtrl.getMaterial);
+api.put("/material", auth, MaterialCtrl.updateMaterial);
+api.delete("/material", auth, MaterialCtrl.deleteMaterial);
