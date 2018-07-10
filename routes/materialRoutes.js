ecfdb0a3c659e5df9b0e61d35ff0bdd572c4fd3c
Douglas_HdezT <00004516@uca.edu.sv>
2018-07-10 12:38:38 -0600

    materialroutes

--- routes/materialRoutes.js
+++ routes/materialRoutes.js
@@ -8,3 +8,5 @@ api.post("/material", auth, MaterialCtrl.insertMaterial);
 api.get("/material", auth, MaterialCtrl.getMaterial);
 api.put("/material", auth, MaterialCtrl.updateMaterial);
 api.delete("/material", auth, MaterialCtrl.deleteMaterial);
+
+module.exports=api;
