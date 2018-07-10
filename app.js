735f537b47f6e66a567fbc8e5143e898aa4c9cbf
Douglas_HdezT <00004516@uca.edu.sv>
2018-07-10 12:39:29 -0600

    app

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const routesProduct = require("./routes/productRoutes")
 const routesAuth = require("./routes/authRoutes")
 const routesUser = require("./routes/userRoutes")
 const routesTicket = require("./routes/ticketRoutes")
+const routesMaterial = require("./routes/materialRoutes")
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -17,5 +18,6 @@ app.use("/api", routesProduct)
 app.use("/auth", routesAuth)
 app.use("/api", routesUser)
 app.use("/api", routesTicket)
+app.use("/api", routesMaterial)
 
 module.exports=app;
