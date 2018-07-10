25056d7603223e7fd05fba68e3d4ec14b4410ad8
Douglas_HdezT <00004516@uca.edu.sv>
2018-07-10 10:35:35 -0600

    route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,7 @@
 'use strict'
 const express = require("express");
 const ProductCtrl = require('../controllers/productController')
-const auth = require('../middlewares')
+const auth = require('../middleware')
 const api = express.Router();
 
 api.post("/product", ProductCtrl.insertNewProduct)
