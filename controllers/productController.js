b10533070466812ed06bf966f13fa1a05c49c712
Douglas_HdezT <00004516@uca.edu.sv>
2018-07-10 10:30:04 -0600

    Insertar producto

---
+++ controllers/productController.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Product = require('../models/productModel');
+
+function getAllProducts(req, res){
+
+}
+
+function insertNewProduct(req, res){
+    let newProduct = new Product();
+    let bd = req.body;
+
+    newProduct.name =  bd.name;
+    newProduct.cost =  bd.cost;
+    newProduct.price =  bd.price;
+    newProduct.stock =  bd.stock;
+    newProduct.sold =  bd.sold;
+    newProduct.category =  bd.category;
+    newProduct.product_materials =  bd.product_materials;
+
+    newProduct.save((err, productSvd)=>{
+        if(err) return res.status(500).send({message:"Internal Error"});
+
+        res.status(200).send(productSvd);
+    });
+
+}
+
+module.exports={
+    insertNewProduct,
+    getAllProducts
+}
