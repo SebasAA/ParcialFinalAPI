7a529dcd3b11053b64511bd6f8dbfe752afd418c
kv519 <kevin.vela519@gmail.com>
2018-07-10 15:35:02 -0600

    Little update in ticket controller

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -3,19 +3,12 @@
 const Ticket = require('../models/ticketModel');
 
 function getTicket(req,res){
-
     let ticketId = req.params.id_tickets;
     
-    Ticket.findById(ticketId,(err,ticket)=>{
+    Ticket.findById(ticketId, (err,ticket)=>{
+        if(err) return res.status(500).send({message: `Something is wrong!`});
+        if(!ticket) return res.status(404).send("Ticket not found");
         
-        if(err) return res.status(500).send({
-            message: `Something is wrong!`
-        });
-
-        if(!ticket) return res.status(404).send({
-            message: `No tickets`
-        });
-
         res.status(200).send(ticket);
     });
 }
@@ -33,13 +26,11 @@ function updateTicket(req, res){
     });
 };
 
-function insertTicket(req,res){
+function insertTicket(req, res){
     let ticket = Ticket(req.body);
 
-    ticket.save((err,ticketStg)=>{
-        if(err) return res.status(500).send({
-            message: `Something is wrong!`
-        });
+    ticket.save((err,ticketStg) => {
+        if(err) return res.status(500).send({ message: `Something is wrong!` });
         res.status(200).send(ticketStg);
     });
 }
