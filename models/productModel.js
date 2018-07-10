575ed0a38b3d568c6b62845f80a6c624ebd826df
Douglas_HdezT <00004516@uca.edu.sv>
2018-07-10 09:14:17 -0600

    productModel

---
+++ models/productModel.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = Schema({
+    name:{
+        type: String,
+        required: true
+    },
+
+    cost:{
+        type: number,
+        required: true
+    },
+
+    price:{
+        type: number,
+        required: true
+    },
+
+    stock:{
+        type: number,
+        required: true
+    },
+
+    sold:{
+        type: number,
+        required: true
+    },
+
+    category:{
+        type: String,
+        required: true,
+        enum:["accesorios","tazas","textiles","artesanales", "otros"]
+    },
+
+    product_materials:[{
+        id_material: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'Material',
+            required: true
+        },
+        quantity:{
+            type: number,
+            required:true
+        }
+    }]
+
+});
